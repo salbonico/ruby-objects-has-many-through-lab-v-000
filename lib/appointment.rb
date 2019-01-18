33ef7bb60a0e59fddc7ad2b8aabcea71bb1f2fa3
Steve Albonico <salbonico@gmail.com>
2019-01-18 23:48:47 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+@@all = []
+
+attr_accessor :date, :patient, :doctor
+
+def initialize(date, patient, doctor)
+  @date = date
+  @patient = patient
+  @doctor = doctor
+  @@all << self
+
+
+
+end
